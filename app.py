a189e4d9efb9b147675a7cd612bf2ac7b10a7fe0
yoneta9 <96574019+yoneta9@users.noreply.github.com>
2023-07-11 23:32:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,3 +67,109 @@ if st.session_state.submit_btn:
       st.caption("検索に時間がかかるため検索結果が50件程度で実行されることをおすすめします。")
       if st.form_submit_button("実行する"):
         st.session_state.search_btn = True
+
+def scrape_data(search_word):
+  base_url = "https://pubmed.ncbi.nlm.nih.gov"
+  search_url = base_url + "/?term=" + search_word
+  articles = []
+
+
+  response = requests.get(search_url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  value_span = soup.find('span', class_='value')
+  value_span_num_int = int(value_span.get_text())
+  value_span_nums = math.ceil(value_span_num_int/10)
+  print('{0}件ヒットした'.format(value_span_num_int))
+  for value_span_num in range(1,value_span_nums+1):
+    search_url2 = search_url + '&page=' +str(value_span_num)
+    # search_url2 = search_url + '&page=1'
+    print('{0}ページ目をスクレイピング中'.format(value_span_num))
+    response = requests.get(search_url2)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    sleep(2)
+    # soup3 =BeautifulSoup(driver.page_source,"html.parser")
+    content = soup.find("div", class_="search-results-chunk results-chunk")
+    for article in content.find_all('a', class_='docsum-title'):
+        article_url = base_url + article.get('href')
+        article_response = requests.get(article_url)
+        article_soup = BeautifulSoup(article_response.text, 'html.parser')
+        article_soup2 = article_soup.find("main", class_="article-details")
+        sleep(2)
+        try:
+            title = article_soup2.find('h1', class_='heading-title').get_text(strip=True)
+        except AttributeError:
+            title = ""
+
+        try:
+            author = article_soup2.find('div', class_='authors-list').get_text(strip=True)
+        except AttributeError:
+            author = ""
+
+        try:
+            abstract = article_soup2.find('div', class_='abstract-content selected').get_text(strip=True)
+        except AttributeError:
+            abstract = ""
+
+        try:
+            publishid = article_soup2.find('span', class_="cit").get_text(strip=True)
+        except AttributeError:
+            publishid = ""
+
+        try:
+            pmid = article_soup2.find('strong', class_="current-id").get_text(strip=True)
+        except AttributeError:
+            pmid = ""
+
+        articles.append([title, author, abstract,publishid,pmid,article_url])
+  df = pd.DataFrame(articles, columns=['title', 'author', 'abstract','Published','PMID','URL'])
+
+  #翻訳していく
+  def translate_text(x):
+    if x ==np.nan:
+      return x
+    else:
+      x = model.translate(x, target_lang='ja')
+      
+      return x
+  df['タイトル'] = df['title'].apply(translate_text)
+  df['要約'] = df['abstract'].apply(translate_text)
+  return df
+
+if st.session_state.search_btn:
+  st.text("それでは引き続きデータの集約と翻訳を進めて参ります。")
+  #from scrape import scrape_data
+  df = scrape_data(st.session_state.search_word)
+  df['Published_year'] = df['Published'].str[0:4]
+  df = df.sort_values('Published_year')
+  fig, ax = plt.subplots(figsize=(8,4))
+  ax.set_title('publish monthly graph')
+  sns.countplot(data=df, x="Published_year",ax=ax)
+  st.pyplot(fig)
+  df1 = df[["タイトル","要約","Published_year","URL"]]
+  st.text("タイトル・要約等を下記にまとめました。隠れている箇所は左ダブルクリックいただくと詳細を確認できます。")
+  st.dataframe(df1)
+  if len(df1)>0:
+    report_table = np.nan
+    st.session_state.report_table  = True
+
+if st.session_state.report_table:
+  with st.form(key="output_form"):
+    st.text("検索結果はいかがでしたでしょうか？　保存されたい方は、下記に保存ファイル名をご入力ください")
+    search_title = st.text_input("作成するファイル名を入力してください")
+    if st.form_submit_button("CSVデータ作成"):
+      st.session_state.output_btn = True
+    st.caption("「Pubmed_{指定されたファイル名}_{本日の日付}.csv」という形で出力されます。")
+    st.caption("ファイルの作成には少し時間がかかります")
+
+if st.session_state.output_btn:
+  if search_title:
+      csv_date = datetime.datetime.today().strftime("%Y%m%d")
+      csv_file_name = "Pubmed_" + search_title +"_" +csv_date + ".csv"
+
+      csv = df.to_csv(index=False)
+      b64 = pybase64.b64encode(csv.encode('utf-8-sig')).decode()
+      href = f'<a href="data:application/octet-stream;base64,{b64}" download={csv_file_name}>Download Link</a>'
+      st.markdown(f"CSVファイルのダウンロード(utf-8 BOM):  {href}", unsafe_allow_html=True)
+  else:
+      st.text("保存ファイル名を入力してください")
